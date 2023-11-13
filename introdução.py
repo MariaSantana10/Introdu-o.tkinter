e422dd5a4b57bfe2d6494e3a34925ff3fc43811e
MariaSantana10 <133781356+MariaSantana10@users.noreply.github.com>
2023-11-13 07:56:31 -0300

    Update introdução.py

--- introdução.py
+++ introdução.py
@@ -1,15 +1,22 @@
 import requests
 from tkinter import *
 def começo():
-    print('olá, vc esta conseguindo')
+    texto= 'olá, vc esta conseguindo'
+    text_exibir['text']=texto
+
 janela = Tk() #abrir janela
 
 janela.title('Experimento')
+janela.geometry('400x400')
+
 
 informaçoes_janela= Label(janela, text = 'To aprendendo')
-informaçoes_janela.grid(column=0,row=0)
+informaçoes_janela.grid(column=0,row=0,padx=10,pady=10)
 
 botao=Button(janela, text = 'clique aqui', command=começo)
-botao.grid(column=0,row=1)
+botao.grid(column=0,row=1,padx=10,pady=10)
+
+text_exibir= Label(janela, text='')
+text_exibir.grid(column=0,row=2,padx=10,pady=10)
 
 janela.mainloop() #para fechar a janela (fica no final do codigo)
